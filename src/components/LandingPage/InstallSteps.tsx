558da16844713f9db8e48980537000c48cdd48f7
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-08-20 10:14:27 +0900

    WIP: landing page

--- src/components/LandingPage/InstallSteps.tsx
+++ src/components/LandingPage/InstallSteps.tsx
@@ -7,17 +7,17 @@ export const InstallSteps: VFC = () => {
       <div>
         <div className="flex flex-row">
           <div className="hidden md:flex flex-col items-center">
-            <div className="w-32 py-5 border border-gray-300 rounded mr-4 flex flex-col items-center justify-center">
+            <div className="w-32 py-5 border border-green-300 rounded mr-4 flex flex-col items-center justify-center">
               <div className="text-3xl font-black text-gray-500 uppercase">
                 Step 1
               </div>
               <div className="text-gray-500 text-sm">on Vercel</div>
             </div>
             <div className="h-full border-l-4 border-transparent">
-              <div className="border-l-4 mr-4 h-full border-gray-300 border-dashed" />
+              <div className="border-l-4 mr-4 h-full border-green-300 border-dashed" />
             </div>
           </div>
-          <div className="flex-auto border rounded  border-gray-300">
+          <div className="flex-auto border rounded  border-green-300">
             <div className="flex md:flex-row flex-col items-center">
               <div className="flex-1">
                 <div className="md:hidden text-sm font-normal pt-3 pl-3 text-gray-500">
@@ -45,26 +45,26 @@ export const InstallSteps: VFC = () => {
         </div>
         <div className="flex items-start flex-row">
           <div className="border-t-4 border-r-4 border-transparent">
-            <div className="w-16 ml-16 h-16 border-l-4 border-gray-300 border-dashed border-b-4 rounded-bl-full" />
+            <div className="w-16 ml-16 h-16 border-l-4 border-green-300 border-dashed border-b-4 rounded-bl-full" />
           </div>
           <div className="border-t-4 border-transparent flex-auto">
-            <div className="h-16 border-b-4 border-gray-300 border-dashed" />
+            <div className="h-16 border-b-4 border-green-300 border-dashed" />
           </div>
-          <div className="w-16 mt-16 mr-16 h-16 border-r-4 border-gray-300 border-dashed border-t-4 rounded-tr-full" />
+          <div className="w-16 mt-16 mr-16 h-16 border-r-4 border-green-300 border-dashed border-t-4 rounded-tr-full" />
         </div>
         <div className="flex flex-row-reverse">
           <div className="hidden md:flex flex-col items-center">
-            <div className="w-32 py-5 border border-gray-300 rounded ml-4 flex flex-col items-center justify-center">
+            <div className="w-32 py-5 border border-green-300 rounded ml-4 flex flex-col items-center justify-center">
               <div className="text-3xl font-black uppercase text-gray-500">
                 Step 2
               </div>
               <div className="text-gray-500 text-sm">on Vercel</div>
             </div>
             <div className="h-full border-r-4 border-transparent">
-              <div className="border-l-4 ml-4 h-full border-gray-300 border-dashed" />
+              <div className="border-l-4 ml-4 h-full border-green-300 border-dashed" />
             </div>
           </div>
-          <div className="flex-auto border rounded border-gray-300">
+          <div className="flex-auto border rounded border-green-300">
             <div className="flex md:flex-row flex-col items-center">
               <div className="flex-1">
                 <div className="md:hidden text-sm font-normal pt-3 pl-3 text-gray-500">
@@ -106,26 +106,26 @@ export const InstallSteps: VFC = () => {
         </div>
         <div className="flex items-start flex-row-reverse">
           <div className="border-t-4 border-l-4 border-transparent">
-            <div className="w-16 mr-16 h-16 border-r-4 border-gray-300 border-dashed border-b-4 rounded-br-full" />
+            <div className="w-16 mr-16 h-16 border-r-4 border-green-300 border-dashed border-b-4 rounded-br-full" />
           </div>
           <div className="border-t-4 border-transparent flex-auto">
-            <div className="h-16 border-b-4 border-gray-300 border-dashed" />
+            <div className="h-16 border-b-4 border-green-300 border-dashed" />
           </div>
-          <div className="w-16 mt-16 ml-16 h-16 border-l-4 border-gray-300 border-dashed border-t-4 rounded-tl-full" />
+          <div className="w-16 mt-16 ml-16 h-16 border-l-4 border-green-300 border-dashed border-t-4 rounded-tl-full" />
         </div>
         <div className="flex flex-row">
           <div className="hidden md:flex flex-col items-center">
-            <div className="w-32 py-5 border border-gray-300 rounded mr-4 flex flex-col items-center justify-center">
+            <div className="w-32 py-5 border border-green-300 rounded mr-4 flex flex-col items-center justify-center">
               <div className="text-3xl font-black uppercase text-gray-500">
                 Step 3
               </div>
               <div className="text-gray-500 text-sm">project code</div>
             </div>
             <div className="h-full border-l-4 border-transparent">
-              <div className="border-l-4 mr-4 h-full border-gray-300 border-dashed" />
+              <div className="border-l-4 mr-4 h-full border-green-300 border-dashed" />
             </div>
           </div>
-          <div className="flex-auto border rounded  border-gray-300">
+          <div className="flex-auto border rounded  border-green-300">
             <div className="flex md:flex-row flex-col items-center">
               <div className="flex-1">
                 <div className="md:hidden text-sm font-normal pt-3 pl-3 text-gray-500">
@@ -165,21 +165,21 @@ export const InstallSteps: VFC = () => {
         </div>
         <div className="flex items-start flex-row">
           <div className="border-t-4 border-r-4 border-transparent">
-            <div className="w-16 ml-16 h-16 border-l-4 border-gray-300 border-dashed border-b-4 rounded-bl-full" />
+            <div className="w-16 ml-16 h-16 border-l-4 border-green-300 border-dashed border-b-4 rounded-bl-full" />
           </div>
           <div className="border-t-4 border-transparent flex-auto">
-            <div className="h-16 border-b-4 border-gray-300 border-dashed" />
+            <div className="h-16 border-b-4 border-green-300 border-dashed" />
           </div>
-          <div className="w-16 mt-16 mr-16 h-16 border-r-4 border-gray-300 border-dashed border-t-4 rounded-tr-full" />
+          <div className="w-16 mt-16 mr-16 h-16 border-r-4 border-green-300 border-dashed border-t-4 rounded-tr-full" />
         </div>
         <div className="flex flex-row-reverse">
           <div className="hidden md:flex flex-col items-center">
-            <div className="w-32 py-5 border border-gray-300 rounded ml-4 uppercase flex flex-col items-center justify-center">
+            <div className="w-32 py-5 border border-green-300 rounded ml-4 uppercase flex flex-col items-center justify-center">
               <div className="text-3xl font-black text-gray-500">Step 4</div>
               <div className="text-gray-500 text-sm">Implementation</div>
             </div>
           </div>
-          <div className="flex-auto border rounded  border-gray-300">
+          <div className="flex-auto border rounded  border-green-300">
             <div className="flex md:flex-row flex-col items-center">
               <div className="flex-auto">
                 <div className="md:hidden text-sm font-normal uppercase pt-3 pl-3 text-gray-500">
