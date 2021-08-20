9165d2ed1d82595e2de5d26fcdbc000511d8ad65
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-08-21 01:07:51 +0900

    update header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,41 @@ import { VFC } from 'react'
 import { Header } from '@/components/Header'
 import { Popover } from '@headlessui/react'
 import { LandingPage } from '@/components/LandingPage'
+import { GetServerSideProps } from 'next'
+import { fetchUserAndTeams } from '@/libs/server-side'
+import { Projects, Teams, User } from '@/types'
 
-const Home: VFC = () => {
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
+  const { user, teams } = await fetchUserAndTeams(ctx)
+
+  if (!user)
+    return {
+      props: {
+        teams: teams ?? [],
+        projects: []
+      }
+    }
+
+  return {
+    props: {
+      user,
+      teams: teams ?? [],
+      projects: []
+    }
+  }
+}
+
+type Props = {
+  user?: User
+  teams: Teams
+  projects: Projects
+}
+
+const Home: VFC<Props> = (props) => {
   return (
     <Popover className="relative bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
-        <Header teams={[]} projects={[]} />
+        <Header {...props} />
         <LandingPage />
       </div>
     </Popover>
--- src/pages/setting.tsx
+++ src/pages/setting.tsx
@@ -2,12 +2,41 @@ import { VFC } from 'react'
 import { Header } from '@/components/Header'
 import { Popover } from '@headlessui/react'
 import { SettingForm } from '@/components/SettingForm'
+import { GetServerSideProps } from 'next'
+import { fetchUserAndTeams } from '@/libs/server-side'
+import { Projects, Teams, User } from '@/types'
 
-const Setting: VFC = () => {
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
+  const { user, teams } = await fetchUserAndTeams(ctx)
+
+  if (!user)
+    return {
+      props: {
+        teams: teams ?? [],
+        projects: []
+      }
+    }
+
+  return {
+    props: {
+      user,
+      teams: teams ?? [],
+      projects: []
+    }
+  }
+}
+
+type Props = {
+  user?: User
+  teams: Teams
+  projects: Projects
+}
+
+const Setting: VFC<Props> = (props) => {
   return (
     <Popover className="relative bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
-        <Header teams={[]} projects={[]} />
+        <Header {...props} />
 
         <div className="mt-8">
           <SettingForm />
