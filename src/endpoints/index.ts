6ff23f3c1d11f6b7914459bda5de22e2c1e4c1f7
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-09-30 10:35:56 +0900

    update api endpoint

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -1,6 +1,6 @@
 export const ENDPOINTS = {
   user: 'https://api.vercel.com/www/user',
-  teams: 'https://api.vercel.com/v13/teams',
+  teams: 'https://api.vercel.com/v1/teams',
   projects: 'https://vercel.com/api/v2/projects',
   environments: (projectId: string): string =>
     `https://api.vercel.com/v8/projects/${projectId}/env/`,
